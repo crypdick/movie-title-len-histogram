da5ce6eec4c1e8db63ed4278890581a1d7bf2749
Richard Decal <richard.decal@gmail.com>
2017-10-07 23:55:28 -0400

    created seaborn histogram, moved psycopg dependency into secret script

--- psycopg_hist.py
+++ psycopg_hist.py
@@ -1,9 +1,22 @@
-import psycopg2
 from secret_open_connection import secret_connection
+import numpy as np
+import seaborn as sns
+
 conn = secret_connection()
 cur = conn.cursor()
 
-cur.execute("select length(title) from movies limit 5;")
-t = cur.fetchall()
-for m in t :
-    print( m[0] )
+cur.execute("select length(title) from movies;")
+num_titles = cur.rowcount
+title_lengths = cur.fetchall()
+
+length_array = np.empty(num_titles)
+
+i=0
+for length in title_lengths:
+    length_array[i] = length[0]
+    i += 1
+
+ax = sns.distplot(length_array, kde=False)
+ax.set_title("Histogram of title lengths in movie dataset")
+ax.set_xlabel("Length of title")
+sns.plt.show()
