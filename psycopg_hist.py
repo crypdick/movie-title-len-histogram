bcffdbdd8c4207f311815c3b358b2ed6f3f4290b
Richard Decal <richard.decal@gmail.com>
2017-10-07 23:33:45 -0400

    created generator that prints out tuple of movie lengths

--- psycopg_hist.py
+++ psycopg_hist.py
@@ -3,7 +3,7 @@ from secret_open_connection import secret_connection
 conn = secret_connection()
 cur = conn.cursor()
 
-cur.execute("select * from movies limit 5;")
+cur.execute("select length(title) from movies limit 5;")
 t = cur.fetchall()
 for m in t :
-    print( m[1] )
+    print( m )
